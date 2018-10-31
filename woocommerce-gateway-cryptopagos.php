d1584a4817e7ff54f551b327b95f426a0bfd532b
Encel Sanchez <encels@gmail.com>
2018-10-31 11:25:20 -0400

    plugin init

---
+++ woocommerce-gateway-cryptopagos.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: WooCommerce Cryptopagos Payment Gateway
+ * Plugin URI: https://cryptopagos-payment.dreamlopers.com
+ * Description: A payment gateway for Cryptopagos
+ * Author: Dreamlopers
+ * Author URI: http://dreamlopers.com
+ * Version: 0.1
+
+ *
+ */
